c62facb74599807916e59e44b06cd518956fc4a0
Alexandra Frock <hosted@alexandra.moe>
2017-04-17 02:10:08 -0400

    Since we require numerical group IDs now, we need to fix this

--- lua/sgrf/client/cl_netmessages.lua
+++ lua/sgrf/client/cl_netmessages.lua
@@ -1,5 +1,5 @@
 net.Receive('SGRF_OpenSteamGroup', function()
-	gui.OpenURL('http://steamcommunity.com/groups/' .. net.ReadString())
+	gui.OpenURL('http://steamcommunity.com/gid/' .. net.ReadString())
 end)
 
 net.Receive('SGRF_ColoredChatPrint', function()
