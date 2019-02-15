27d7b2355f611340ed5e72f4200c8b26b38f0621
Eliot Partridge <shortcircuit369@gmail.com>
2019-02-15 12:26:48 -0600

    Correct traversal

--- lua/sgrf/server/sv_functions.lua
+++ lua/sgrf/server/sv_functions.lua
@@ -70,14 +70,14 @@ local function recursiveXMLFallback(ply, xmlUrl, callback)
 			SGRF.Log('DEBUG', 'Beginning body traversal...')
 			for k, child in pairs(doc.kids) do
 				SGRF.Log('DEBUG', '%d: %s (%s) encountered', k, child.name, child.type)
-				if child.type == 'element' and child.name == 'nextPageLink' then
-					SGRF.Log('DEBUG', 'Found next page link')
-					nextPageLink = child.kids[0].value
-				elseif child.type == 'element' and child.name == 'memberList' then -- root element
+				if child.type == 'element' and child.name == 'memberList' then -- root element
 					SGRF.Log('DEBUG', 'Found root element')
 					for k2, child2 in pairs(child.el) do
 						SGRF.Log('DEBUG', '%d - %d: %s (%s) encountered', k, k2, child2.name, child2.type) -- shut up about my code pyramids glualint :(
-						if child2.name == 'members' then -- members list
+						if child.name == 'nextPageLink' then
+							SGRF.Log('DEBUG', 'Found next page link')
+							nextPageLink = child.kids[0].value
+						elseif child2.name == 'members' then -- members list
 							SGRF.Log('DEBUG', 'Found members element')
 							for k3, member in pairs(child2.el) do
 								SGRF.Log('DEBUG', '%d - %d - %d: %s (%s) encountered', k, k2, k3, member.name, member.type)
